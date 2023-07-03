9c26b8acb503f53c2efb6e8a92d1b7898f8692c7
AlonRTPve <94314848+AlonRTPve@users.noreply.github.com>
2023-07-03 17:25:55 +0300

    Version 0.9 - Not completed

--- Chatapp.py
+++ Chatapp.py
@@ -1,48 +1,71 @@
-
 from PyQt6.QtWidgets import *
 from PyQt6 import uic
-import socket, sys, threading
+import socket
+import sys
+import threading
 
 IP, PORT = "127.0.0.1", 65432
 SERVERADDR = (IP, PORT)
 FORMAT = "utf-8"
 
 
-class Loginpage(QMainWindow):
 
+class Socket():
     def __init__(self):
-        super(Loginpage, self).__init__()
+        self.connect_to_server()
+
+
+    def connect_to_server(self):
+        """
+        Function that initializes a connection with the server.
+        """
+        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientsocket.connect(SERVERADDR)
+
+
+
+    def receive_message(self):
+        try:
+            message = self.clientsocket.recv(1024).decode(FORMAT)
+            print(f"[Client] received message {message}")
+            return message
+        except Exception as e:
+            print("Err;or receiving message:", str(e))
+            return None
+
+    def send_message(self, message):
+        try:
+            if message:
+                print(f"[Client] Sending message {message}")
+                self.clientsocket.sendall(message.encode())
+        except ConnectionResetError as e:
+            print("Error sending message:", str(e))
+
+
+class LoginPage(QMainWindow):
+    def __init__(self):
+        super(LoginPage, self).__init__()
         uic.loadUi("LoginPage.ui", self)
         self.show()
 
-        ##Variables
-
-
-        ##Actions
-        self.connect_to_server()
+        # Actions
         self.loginButton.clicked.connect(self.login)
         self.registerButton.clicked.connect(self.register)
 
-
     def login(self):
-
         self.username, self.password = self.loginEntry.text(), self.passwordEntry.text()
         self.passwordEntry.clear()
 
         if self.username and self.password:
-
-            self.send_msg(f"LOGIN|{self.username}:{self.password}")
-            received_message = self.receive_msg()
+            server_socket.send_message(f"LOGIN|{self.username}:{self.password}")
+            received_message = server_socket.receive_message()
             if received_message == "login successful":
-
                 self.alertLabel.setStyleSheet('color: green')
                 self.alertLabel.setText("Login Successful")
-                self.open_second_screen()
+                self.open_lobby_page()
                 return
-
             elif received_message == "user already logged in":
-
-                self.alertLabel.setText("user already logged in")
+                self.alertLabel.setText("User already logged in")
                 self.alertLabel.setStyleSheet('color: red')
                 return
             else:
@@ -51,86 +74,40 @@ class Loginpage(QMainWindow):
 
 
     def register(self):
-
         self.username, self.password = self.loginEntry.text(), self.passwordEntry.text()
         self.passwordEntry.clear()
 
         if self.username and self.password:
-
-            self.send_msg(f"REGISTER|{self.username}:{self.password}")
-
-            if self.receive_msg() == "registration successful":
-
+            server_socket.send_message(f"REGISTER|{self.username}:{self.password}")
+            if server_socket.receive_message() == "registration successful":
                 self.alertLabel.setStyleSheet('color: green')
                 self.alertLabel.setText("Register Successful")
-                return
-
-            self.alertLabel.setText("user already exists ")
-            self.alertLabel.setStyleSheet('color: red')
-
-
-    def connect_to_server(self):
-        """
-        Function that initializes a connection with the server.
-
-        """
-
-        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientsocket.connect(SERVERADDR)
+            else:
+                self.alertLabel.setText("User already exists")
+                self.alertLabel.setStyleSheet('color: red')
 
 
-    def receive_msg(self):
-        clientsocket = self.clientsocket
-        try:
-            message = clientsocket.recv(1024).decode(FORMAT)
-            return message
-        except Exception:
-            return
-
-
-    def send_msg(self, message):
-        clientsocket = self.clientsocket
-        try:
-            if message:
-                clientsocket.sendall(message.encode())
-        except ConnectionResetError:
-            return
-
-
-    def open_second_screen(self):
+    def open_lobby_page(self):
         self.hide()
-        self.lobbypage = LobbyPage()
-        self.lobbypage.show()
+        self.lobby_page = LobbyPage(self.username)
+        self.lobby_page.show()
 
 
-class LobbyPage(Loginpage, QMainWindow):
-
-    def __init__(self):
+class LobbyPage(QMainWindow):
+    def __init__(self, username):
         super(LobbyPage, self).__init__()
         uic.loadUi("chooseRoomPage.ui", self)
 
-        ##Variables
-        username = login_window.username
-        self.clientsocket = login_window.clientsocket
+        # Variables
+        self.username = username
+        self.logged_users = None
 
-        ##Actions
+        # Actions
         self.createRoomButton.clicked.connect(self.create_room)
         self.joinRoomButton.clicked.connect(self.join_room)
-        self.nameLabel.setText(f"{username}!")
-
-
-
+        self.nameLabel.setText(f"{self.username}!")
 
     def join_room(self):
-        """
-              Steps:
-              1. Once button is pressed get the room code entry
-              2. Send entry to server to check if room exists
-              3. join room
-              return
-
-              """
-
         self.joinRoomAlertLabel.setText("")
         self.room_code = self.joinRoomLabel.text()
 
@@ -138,28 +115,15 @@ class LobbyPage(Loginpage, QMainWindow):
             self.joinRoomAlertLabel.setText("Room name must be longer than 3")
             return
 
-        self.send_msg(f"JOINROOM|{self.room_code}")
-        server_response = self.receive_msg()
+        server_socket.send_message(f"JOINROOM|{self.room_code}")
+        server_response = server_socket.receive_message()
         if server_response == "success":
-            self.logged_users = self.receive_msg()
-            self.open_chat_window()
+            self.logged_users = server_socket.receive_message()
+            self.open_chat_page()
         else:
-            self.joinRoomAlertLabel.setText("incorrect room code")
-
-
-
-
+            self.joinRoomAlertLabel.setText("Incorrect room code")
 
     def create_room(self):
-        """
-        Steps:
-        1. Once button is pressed get the room code entry
-        2. Send entry to server to check if room exists
-        3. if room doesn't exist create room
-        return
-
-        """
-
         self.createRoomAlertLabel.setText("")
         self.room_code = self.createRoomLabel.text()
 
@@ -167,83 +131,88 @@ class LobbyPage(Loginpage, QMainWindow):
             self.createRoomAlertLabel.setText("Room name must be longer than 3")
             return
 
-        self.send_msg(f"CREATEROOM|{self.room_code}")
-        server_response = self.receive_msg()
+        server_socket.send_message(f"CREATEROOM|{self.room_code}")
+        server_response = server_socket.receive_message()
 
         if server_response == "created room!":
-            self.open_chat_window()
+            self.open_chat_page()
         else:
             self.createRoomAlertLabel.setText("Room already exists")
 
 
-    def open_chat_window(self):
+    def open_chat_page(self):
         self.hide()
-        self.Chatpage = ChatPage()
-        self.Chatpage.show()
+        self.chat_page = ChatPage(self.room_code, self.username, self.logged_users)
 
 
-
-class ChatPage(Loginpage, QMainWindow):
-
-    def __init__(self):
+class ChatPage(QMainWindow):
+    def __init__(self, roomcode, username, logged_users=None):
         super(ChatPage, self).__init__()
         uic.loadUi("ChatPage.ui", self)
+        self.show()
 
-        ##Variables
-        room_code = login_window.lobbypage.room_code
+        # Variables
+        self.room_code = roomcode
+        self.username = username
+        self.logged_users = logged_users
 
-
-        ##Actions
-        self.sendButton.clicked.connect(self.send_message)
-        self.RoomLabel.setText(f"Room Id# {room_code}")
-        self.append_users()
-        socket_thread = threading.Thread(target=self.handle_socket)
+        # Actions
+        self.usersFrame.append(self.username)
+        self.RoomLabel.setText(f"Room Id: {self.room_code}")
+        socket_thread = threading.Thread(target=self.receive_and_handle_messages)
         socket_thread.start()
 
+        self.sendButton.clicked.connect(self.send_message)
+        if logged_users:
+            self.append_users()
 
 
-    def handle_socket(self):
+
+    def receive_and_handle_messages(self):
+        """
+        Possible MESSAGES CODES: message, userleave, userjoin
+
+        :return:
+        """
+
         while True:
-            print("in the thread: ")
-            try:
-                message = self.clientsocket.recv(1024).decode()
-                print("in the thread: " + message)
-                if not message:
-                    pass
-            except ConnectionError:
+            print("receiving")
+            message = server_socket.receive_message()
+            message_code, message_data = message.split("|")[0], message.split("|")[1]
+            if message_code == "userjoin":
+                self.self.append_user(message_data)
+            elif message_code == "userleave":
+                continue
+            elif message_code == "message":
                 pass
 
+
     def send_message(self):
-        login_window.send_msg("MESSAGE|hey")
+        self.message = self.chatLabel.text()
+        if self.message:
+            server_socket.send_message(self.message)
+
 
     def append_users(self):
-        username = login_window.username
+        ###THIS FUNCTION IS Mainly for the list of the users, created another function just to append a single user.
         try:
-            logged_users = login_window.lobbypage.logged_users
-            list_of_logged_users = logged_users.split("|")
-            list_of_logged_users.remove(username)
+            list_of_logged_users = self.logged_users.split("|")
+            list_of_logged_users.remove(self.username)
             for user in list_of_logged_users:
                 if user:
                     self.usersFrame.append(user)
-            self.usersFrame.append(username)
-        except AttributeError:
-            self.usersFrame.append(username)
+            self.usersFrame.append(self.username)
+        except AttributeError as e:
+            print("Error receiving message:", str(e))
+            self.usersFrame.append(self.username)
 
 
-
-
-
-
-
-
-
-
-
-
-
+    def append_user(self, user):
+        self.usersFrame.append(user)
 
 
 if __name__ == "__main__":
+    server_socket = Socket()
     app = QApplication(sys.argv)
-    login_window = Loginpage()
+    login_page = LoginPage()
     sys.exit(app.exec())
--- Server.py
+++ Server.py
@@ -4,164 +4,182 @@ import Databasemanager as Database
 
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65432  # Port to en on (non-privileged ports are > 1023)
 SIZE = 1024
 DISCONNECT_MSG = "/DISCONNECT"
 FORMAT = "utf-8"
-connected_clients = []
-logged_users = {}
-rooms = {}
 
-def handle_client(conn, addr):
-    print(f"[SERVER] {addr} has connected to the server \n")
-    connected = True
-    while connected:
+
+####TO FIX
+
+
+
+class ChatServer:
+    def __init__(self):
+        self.logged_users = {}
+        self.rooms = {}
+        self.room_code = None
+
+        self.start_server_socket()
+
+
+    def start_server_socket(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates a new socket object
+        self.server_socket.bind((HOST, PORT))  # bind the SERVER HOST AND PORT
+        self.server_socket.listen()  # starts listening for clients
+        print(f"[SERVER] Server is listening on {HOST}:{PORT}")
+
+        while True:
+            conn, addr = self.server_socket.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}\n")
+
+    def receive_message(self, conn):
         try:
             client_message = conn.recv(SIZE).decode(FORMAT)
+            if client_message:
+                print(f"[SERVER] Received message: {client_message}")
+                return client_message
+
+        except (ConnectionResetError, OSError, TypeError):
+            self.remove_client()
+
+
+    def handle_client(self, conn, addr):
+        self.conn, self.addr = conn, addr
+        print(f"[SERVER] {self.addr} has connected to the server \n")
+        self.connected = True
+
+        while self.connected:
+            self.client_message = self.receive_message(self.conn)
+            self.message_code, self.message_data = self.split_message()
+            if self.message_code and self.message_data:
+                self.handle_message_codes()
+
+
+    def split_message(self):
+        try:
+            if "|" in self.client_message:
+                message_code, message_data = self.client_message.split("|", 1)
+                return message_code, message_data
+            return None, None
+        except Exception:
+            self.remove_user()
+            return None, None
+
+
+    def check_if_user_logged(self):
+        self.username = self.message_data.split(":")[0]
+        for value in self.logged_users.values():
+            if value == self.username:
+                return True
+
+        self.logged_users[self.conn] = self.username
+        return False
+
+
+    def remove_user(self):
+        self.logged_users.pop(self.conn, None)
+
+
+    def get_all_users_from_room(self):
+        string_of_users = ""
+        for key, value in self.logged_users.items():
+            for socket in self.rooms[self.room_code]:
+                if socket == key:
+                    string_of_users += f"{value}|"
+        return string_of_users
+
+
+    def delete_user_from_room(self):
+        if self.room_code in self.rooms:
+            if self.conn in self.rooms[self.room_code]:
+                self.rooms[self.room_code].remove(self.conn)
+            if len(self.rooms[self.room_code]) == 0:
+                del self.rooms[self.room_code]
+                print("Room deleted as it became empty.")
+            else:
+                print("User is not in the room.")
+        else:
+            print("Room does not exist.")
+
+
+    def send_message(self, message):
+        try:
+            print(f"[SERVER] Sending message: {message}")
+            self.conn.send(message.encode())
         except ConnectionResetError:
+            print("[ERROR] Connection reset by peer")
+            self.remove_client()
+        except socket.error as e:
+            print(f"[ERROR] Failed to send message: {e}")
+            self.remove_client()
+        except Exception as e:
+            print(f"[ERROR] An unexpected error occurred while sending message: {e}")
+            self.remove_client()
 
-            try:
-                delete_user_from_room(room_code, conn)
-            except UnboundLocalError:
-                pass
 
-            remove_user(conn)
-            print(f"[SERVER] {addr[0]}:{str(addr[1])} has disconnected from the server")
-            connected_clients.remove(conn)
-            conn.close()
-            break
-
-        if not client_message:
-            connected_clients.remove(conn)
-            print(f"[SERVER] {addr[0]}:{str(addr[1])} has disconnected from the server")
-            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1} ")
-            conn.close()
-            break
-
-        message_code, message_data = split_message(client_message)
-        if message_code == "LOGIN":
-            try:
-                if Database.login(message_data):
-                    if not check_if_user_logged(message_data, conn):
-                        conn.send("login successful".encode())
-                    else:
-                        conn.send("user already logged in".encode())
+    def handle_message_codes(self):
+        if self.message_code == "LOGIN":
+            if Database.login(self.message_data):
+                if not self.check_if_user_logged():
+                    self.send_message("login successful")
                 else:
-                    conn.send("login failed".encode())
-            except Exception as e:
-                pass
-
-
-        elif message_code == "REGISTER":
-            try:
-                conn.send((Database.register(message_data)).encode())
-            except Exception as e:
-                pass
-
-        elif message_code == "CREATEROOM":
-            room_code = message_data
-            for k, value in rooms.items():
-                if k == room_code:
-                    conn.send("room already exists!".encode())
+                    self.send_message("user already logged in")
             else:
+                self.send_message("login failed")
+
+        elif self.message_code == "REGISTER":
+            self.send_message((Database.register(self.message_data)))
+
+        elif self.message_code == "CREATEROOM":
+            self.room_code = self.message_data
+            for k, value in self.rooms.items():
+                if k == self.room_code:
+                    self.send_message("room already exists!")
+            else:
+                self.rooms.setdefault(self.room_code, []).append(self.conn)
+                self.send_message("created room!")
+
+        elif self.message_code == "JOINROOM":
+            for k, value in self.rooms.items():
+                if k == self.message_data:
+                    self.rooms[self.message_data].append(self.conn)
+                    self.send_message("success")
+                    self.room_code = self.message_data
+                    self.send_message(self.get_all_users_from_room())
+                    self.send_message(f"userjoin|{self.username}")
+
+            else:
+                self.send_message("failed")
+
+        elif self.message_code == "MESSAGE":
+            self.broadcast()
+
+    def remove_client(self):
+        self.connected = False
+        print(f"[SERVER] USER {self.addr[0]}:{self.addr[1]} disconnected")
+        self.remove_user()
+        if self.username in self.logged_users:
+            self.logged_users.pop(self.username)
+        if self.room_code:
+            self.delete_user_from_room()
+        self.conn.close()
+
+    def broadcast(self):
+        for client in self.rooms[self.room_code]:
+            if client != self.conn:
                 try:
-                    rooms[room_code].append(conn)
-                    conn.send("created room!".encode())
-                except KeyError:  # this exception is for the first iteration of the program.
-                    rooms[room_code] = [conn]
-                    conn.send("created room!".encode())
-
-
-        elif message_code == "JOINROOM":
-            room_code = message_data
-            for k, value in rooms.items():
-                if k == room_code:
-                    rooms[room_code].append(conn)
-                    conn.send("success".encode())
-                    conn.send(get_all_users_from_room(room_code).encode())
-
-            else:
-                conn.send("failed".encode())
-
-        else:
-            for client in rooms[room_code]:
-                if client != conn:
-                    try:
-                        client.send(message_data.encode())
-                        print(f"[SERVER] {addr[0]}:{str(addr[1])} says {message_data}")
-                    except socket.error as e:
-                        print(f"[SERVER] Error sending message to client: {e}")
-
-
-            #for client in connected_clients:
-            #    if client is not conn:
-            #        client.send(message_code.encode())
-            #        print(f"[SERVER] {addr[0]}:{str(addr[1])} says {message_data}")
-
-
-def split_message(msg):
-    try:
-        message_code, message_data = msg.split("|")[0], msg.split("|")[1]
-        return message_code, message_data
-    except Exception:
-        pass
-
-
-def check_if_user_logged(message_data, conn):
-    username = message_data.split(":")[0]
-    for key, value in logged_users.items():
-        if value == username:
-            return True
-
-    logged_users[conn] = username
-    return False
-
-
-def remove_user(conn):
-    logged_users.pop(conn, None)
-
-
-def get_all_users_from_room(room_code):
-    string_of_users = ""
-    for key, value in logged_users.items():
-        for socket in rooms[room_code]:
-            if socket == key:
-                string_of_users += f"{value}|"
-
-    return string_of_users
-
-
-def delete_room(roomcode):
-    rooms.pop(roomcode)
-
-
-def delete_user_from_room(room_code, conn):
-    rooms[room_code].remove(conn)
-    if room_code in rooms:
-        if conn in rooms[room_code]:
-            rooms[room_code].remove(conn)
-        if len(rooms[room_code]) == 0:
-            del rooms[room_code]
-            print("Room deleted as it became empty.")
-        else:
-            print("User is not in the room.")
-    else:
-        print("Room does not exist.")
-
+                    self.send_message(self.message_data)
+                    print(f"[SERVER] {self.addr[0]}:{str(self.addr[1])} says {self.message_data}")
+                except socket.error as e:
+                    print(f"[SERVER] Error sending message to client: {e}")
 
 def main():
     print("[SERVER] Server is starting... ")
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates a new socket object
-    server_socket.bind((HOST, PORT))  # bind the SERVER HOST AND PORT
-    server_socket.listen()  # starts listening for clients
-    print(f"[SERVER] Server is listening on {HOST}:{PORT}")
+    server = ChatServer()  # Create an instance of ChatServer
 
-    while True:
-        conn, addr = server_socket.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        connected_clients.append(conn)
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}\n")
 
 
 if __name__ == '__main__':
